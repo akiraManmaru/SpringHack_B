4b042d2a6e311766447fd210136aad7a24190c59
akiraManmaru <umi2180@outlook.jp>
2023-03-25 01:07:02 +0900

    Coalescing code

--- main.js
+++ main.js
@@ -3,46 +3,141 @@
  */
 
 
-const btn = document.getElementById('btn');
+// 
 
-    // ボタンがクリックされたら、音符をアニメーションする
-    btn.addEventListener('click', function() {
-      // 音符を作成する
-      const note = document.createElement('div');
-      note.classList.add('note');
-      // ボタンの位置を取得する
-      const btnRect = btn.getBoundingClientRect();
-      // 音符の初期位置をボタンの位置に設定する
-      note.style.left = btnRect.left + 'px';
-      note.style.top = btnRect.top + 'px';
-      // 音符をbody要素に追加する
-      document.body.appendChild(note);
 
-      // アニメーションを開始する
-      const duration = 1500; // アニメーションの時間（ミリ秒）
-      const startX = btnRect.left; // 開始点のx座標
-      const startY = btnRect.top - 60; // 開始点のy座標
-      const endX = btnRect.left + 300; // 終了点のx座標
-      const endY = window.innerHeight; // 終了点のy座標
-      const gravity = 1500; // 重力の大きさ（ピクセル/秒^2）
-      const startTime = Date.now(); // アニメーションの開始時間
+document.querySelector('.cello').animate(
+	[
+		{
+			offset: 0.00,
+			transform: 'translate(0, 0)'
+		},
+		{
+			offset: 0.02,
+			transform: 'rotate(0.01turn)'
+		},
+		{
+			offset: 0.04,
+			transform: 'rotate(-0.01turn)'
+		},
+		{
+			offset: 0.06,
+			transform: 'rotate(0.01turn)'
+		},
+		{
+			offset: 0.10,
+			transform: 'translate(0, 0)'
+		},
+		{
+			offset: 1.00,
+			transform: 'translate(0, 0)'
+		}
+	],
+	{
+		duration: 8000,
+		iterations: Infinity
+	}
+);
 
-      function animate() {
-        const currentTime = Date.now() - startTime; // 現在のアニメーションの時間
-        // if (currentTime >= duration) {
-        //   note.remove();
-        //   return;
-        // }
-        // 現在の位置を計算する
-        const timeFraction = currentTime / duration;
-        const x = startX + (endX - startX) * timeFraction;
-        const y = startY - (endY - startY) * currentTime / duration + 0.5 * 1000 * Math.pow(currentTime / duration, 2);
-        // 音符の位置を設定する
-        note.style.left = x + 'px';
-        note.style.top = y + 'px';
-        // 次のフレームをアニメーションする
-        requestAnimationFrame(animate);
-      }
 
-      requestAnimationFrame(animate);
-    });
+/*
+* ひょっこり音符
+*/
+document.querySelector('.note').animate(
+	[
+		{
+			offset: 0.00,
+			transform: 'translate(0, 0)'
+		},
+		{
+			offset: 0.10,
+			transform: 'translate(-100%, 0)'
+		},
+        {
+			offset: 0.14,
+			transform: 'translate(-100%, 0%)'
+		},
+        {
+			offset: 0.15,
+			transform: 'translate(-100%, -10%)'
+		},
+        {
+			offset: 0.16,
+			transform: 'translate(-100%, 0%)'
+		},
+        {
+			offset: 0.17,
+			transform: 'translate(-100%, -10%)'
+		},
+        {
+			offset: 0.18,
+			transform: 'translate(-100%, 0%)'
+		},
+        {
+			offset: 0.50,
+			transform: 'translate(-100%, 0)'
+		},
+		{
+			offset: 0.60,
+			transform: 'translate(0, 0)'
+		},
+	],
+	{
+		duration: 12000,
+		iterations: Infinity
+	}
+);
+
+
+/**
+ * 走る犬
+ */
+document.querySelector('.inu').animate(
+	[
+		{
+			offset: 0.10,
+			transform: 'translate(0, 0)'
+		},
+    {
+			offset: 0.20,
+			transform: 'translate(-1300px, 0)'
+		},
+		{
+			offset: 0.25,
+			transform: 'translate(-650px, 0)'
+		},
+    {
+			offset: 0.26,
+			transform: 'translate(-645px, 0)'
+		},
+    {
+			offset: 0.27,
+			transform: 'translate(-655px, 0)'
+		},
+    {
+			offset: 0.28,
+			transform: 'translate(-645px, 0)'
+		},
+    {
+			offset: 0.29,
+			transform: 'translate(-655px, 0)'
+		},
+    {
+			offset: 0.30,
+			transform: 'translate(-650px, 0)'
+		},
+    {
+			offset: 0.35,
+			transform: 'translate(0, 0)'
+		},
+	],
+	{
+		duration: 20000,
+		iterations: Infinity
+	}
+);
+
+
+
+
+    
